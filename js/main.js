13c669e8c552d9bcd2dd2ea2fae57ca16506cac7
brandon932 <bthornton_932@yahoo.com>
2015-08-01 23:53:18 -0600

    refactored code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  $(".btn").on("click",function(e){
+  e.preventDefault();
+  var bill = $("#bill").val();
+  var tipAmnt = 0.20;
+  var tip = $("#bill").val() * tipAmnt;
+  $(".tipAmt").html('<h2>You should tip '+tip +' dollars.</h2>');
+  });
 });
