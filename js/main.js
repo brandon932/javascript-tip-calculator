4d9a214f1b2daa868371765b862aa85c652d63c8
brandon932 <bthornton_932@yahoo.com>
2015-08-03 12:01:31 -0600

    added option drop down and regex to take the bill num from a string

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ $(document).on('ready', function() {
   $(".btn").on("click",function(e){
   e.preventDefault();
   var bill = $("#bill").val();
-  var tipAmnt = 0.20;
-  var tip = $("#bill").val() * tipAmnt;
-  $(".tipAmt").html('<h2>You should tip '+tip.toFixed(2) +'</h2>');
+  var  billNum = bill.replace(/[^0-9\.]+/g,'');
+  console.log(billNum);
+  var tipAmnt = $(".tipValue").val();
+  var tip =  billNum * tipAmnt;
+  $(".tipAmt").html('<h2>You should tip $'+tip.toFixed(2) +'</h2>');
   });
 });
