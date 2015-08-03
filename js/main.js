1e71b1de91790cac473e6b402a079991c02c1f3c
brandon932 <bthornton_932@yahoo.com>
2015-08-03 17:23:05 -0600

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-// add scripts
 
-$(document).on('ready', function() {
-  $(".btn").on("click",function(e){
-  e.preventDefault();
-  var bill = $("#bill").val();
-  var  billNum = bill.replace(/[^0-9\.]+/g,'');
-  var tipAmnt = $(".tipValue").val();
-  var tip =  billNum * tipAmnt;
-  if (tip !== 0.00){
-    $(".tipAmt").html('<h2>You should tip $'+tip.toFixed(2) +'</h2>');
-  }else{
-    alert("please enter the bill and select a tip amount");
-    }
+  $(document).on('ready', function() {
+    $(".btn").on("click",function(e){
+    e.preventDefault();
+    var bill = $("#bill").val();
+    var  billNum = bill.replace(/[^0-9\.]+/g,'');
+    var tipAmnt = $(".tipValue").val();
+    var tip =  billNum * tipAmnt;
+    if (tip !== 0.00){
+      $(".tipAmt").html('<h2>You should tip $'+tip.toFixed(2) +'</h2>');
+    }else{
+      alert("please enter the bill and select a tip amount");
+      }
+    });
   });
-});
-
-// TODO: Add a total for bill + tip
