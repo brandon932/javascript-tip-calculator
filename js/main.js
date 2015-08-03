7c628486c9b08e6b593e9d8d6b71263515530ee6
brandon932 <bthornton_932@yahoo.com>
2015-08-03 14:47:28 -0600

    added functionality to prevent a tip of 0.00 and fixed the form being prefilled out on refresh

--- js/main.js
+++ js/main.js
@@ -5,9 +5,14 @@ $(document).on('ready', function() {
   e.preventDefault();
   var bill = $("#bill").val();
   var  billNum = bill.replace(/[^0-9\.]+/g,'');
-  console.log(billNum);
   var tipAmnt = $(".tipValue").val();
   var tip =  billNum * tipAmnt;
-  $(".tipAmt").html('<h2>You should tip $'+tip.toFixed(2) +'</h2>');
+  if (tip !== 0.00){
+    $(".tipAmt").html('<h2>You should tip $'+tip.toFixed(2) +'</h2>');
+  }else{
+    alert("please enter the bill and select a tip amount");
+    }
   });
 });
+
+// TODO: Add a total for bill + tip
